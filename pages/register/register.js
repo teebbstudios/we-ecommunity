cb5868e4e48d7dc8a89f3ea0ad59141bc4f28a63
teebbstudios <qww.zone@gmail.com>
2021-05-02 19:10:58 +0800

    register 4 step

---
+++ pages/register/register.js
@@ -0,0 +1,320 @@
+// pages/register/register.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //所选择的登记类型，0：为自己登记，1：代他人登记
+    type: 0,
+    //登记类型提示
+    typeDescription: '如果您是首次登记，请先为自己登记信息。',
+    types: [{
+        value: 0,
+        name: '为自己登记',
+        checked: 'true',
+        description: '如果您是首次登记，请先为自己登记信息。'
+      },
+      {
+        value: 1,
+        name: '代他人登记',
+        description: '代他人登记之前，请先为自己登记信息。仅允许登记相同户号的他人信息。'
+      }
+    ],
+    numList: [{
+      name: '登记类型'
+    }, {
+      name: '选择住址'
+    }, {
+      name: '填写信息'
+    }, {
+      name: '登记完成'
+    }, ],
+    num: 0,
+    areas: [{
+        id: 1,
+        name: "康乐社区"
+      },
+      {
+        id: 2,
+        name: "紫金花园"
+      },
+      {
+        id: 3,
+        name: "博泰花园"
+      }
+    ],
+    areaId: null,
+    areaIndex: null,
+    buildings: [{
+        id: 1,
+        label: "#1"
+      },
+      {
+        id: 2,
+        label: "#2"
+      },
+      {
+        id: 3,
+        label: "#3"
+      },
+      {
+        id: 4,
+        label: "#4"
+      },
+    ],
+    buildingId: null,
+    buildingIndex: null,
+    rooms: [{
+        id: 1,
+        label: "#101"
+      },
+      {
+        id: 2,
+        label: "#102"
+      },
+      {
+        id: 3,
+        label: "#201"
+      },
+      {
+        id: 4,
+        label: "#202"
+      },
+    ],
+    roomId: null,
+    roomIndex: null,
+    imgList: [],
+    //自拍照
+    selfie: null,
+    selfieTmp: null,
+    //身份证人像面
+    idcardBack: null,
+    idcardBackTmp: null,
+    //身份证国徽面
+    idcardFront: null,
+    idcardFrontTmp: null,
+
+    sexs: ['男', '女'],
+    sex: '男',
+    sexIndex: 0,
+    date: null,
+    marriages: ['单身', '已婚', '离异', '丧偶', '其他'],
+    marriage: null,
+    marriageIndex: null,
+    politicals: ['中共党员', '中共预备党员', '共青团员', '群众', '民革党员', '民盟盟员', '民建会员', '民进会员', '农工党党员', '致公党党员', '九三学社社员', '台盟盟员', '无党派人士'],
+    political: null,
+    politicalIndex: null,
+    educations: ['小学', '初中', '高中', '大专', '本科', '硕士', '博士', '无学历'],
+    education: null,
+    educationIndex: null,
+    address: null,
+  },
+  radioChange: function (e) {
+    this.setData({
+      typeDescription: this.data.types[e.detail.value].description
+    });
+  },
+  numSteps: function (e) {
+    let btnType = e.currentTarget.id;
+    let currentStep = this.data.num;
+    console.log(currentStep)
+    if (btnType === 'prev') {
+      currentStep <= 0 ? 0 : currentStep -= 1;
+    }
+    if (btnType === 'next') {
+      currentStep += 1;
+    }
+    this.setData({
+      num: currentStep
+    })
+  },
+  pickerChange: function (e) {
+    console.log(e);
+    let pickerType = e.currentTarget.id;
+    let pickIndex = e.detail.value;
+    switch (pickerType) {
+      case "area":
+        this.setData({
+          areaIndex: pickIndex,
+          areaId: this.data.areas[pickIndex].id
+        })
+        break;
+      case "building":
+        this.setData({
+          buildingIndex: pickIndex,
+          buildingId: this.data.buildings[pickIndex].id
+        })
+        break;
+      case "room":
+        this.setData({
+          roomIndex: pickIndex,
+          roomId: this.data.rooms[pickIndex].id
+        })
+        break;
+      case "sex":
+        this.setData({
+          sexIndex: pickIndex,
+          sex: this.data.sexs[pickIndex]
+        });
+        break;
+      case "marriage":
+        this.setData({
+          marriageIndex: pickIndex,
+          marriage: this.data.marriages[pickIndex]
+        });
+        break;
+      case "political":
+        this.setData({
+          politicalIndex: pickIndex,
+          political: this.data.politicals[pickIndex]
+        });
+        break;
+      case "education":
+        this.setData({
+          educationIndex: pickIndex,
+          education: this.data.educations[pickIndex]
+        });
+        break;
+    }
+  },
+  dateChange(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  goCamera: function (e) {
+    let cameraType = e.currentTarget.id;
+    switch (cameraType) {
+      case "selfie":
+        wx.navigateTo({
+          url: '/pages/selfie/selfie',
+        })
+        break;
+      case "idcardback":
+        wx.navigateTo({
+          url: '/pages/backidcard/back',
+        })
+        break;
+      case "idcardfront":
+        wx.navigateTo({
+          url: '/pages/frontidcard/front',
+        })
+        break;
+    }
+  },
+
+  getPhoneNumber: function (e) {
+    console.log(e);
+  },
+
+  getChoosePoi: (e) => {
+    wx.choosePoi(e)({
+      success: (e) => {
+        console.log(e);
+      }
+    })
+  },
+
+  //验证是否授权打开地图权限
+  getLocation: function () {
+    wx.getSetting({
+      success: (res) => {
+        // 判断定位的授权
+        if (!res.authSetting['scope.userLocation']) {
+          wx.authorize({
+            scope: 'scope.userLocation',
+            success() {
+              this.onGetLocation();
+            },
+            fail(errMsg) {
+              wx.showToast({
+                title: JSON.stringify(errMsg),
+                icon: 'none'
+              })
+            }
+          })
+        } else {
+          this.onGetLocation();
+        }
+      }
+    })
+  },
+
+  //打开地图获取位置
+  onGetLocation: function (e) {
+    wx.chooseLocation({
+      success: (result) => {
+        console.log(result);
+        this.setData({
+          address: result.address + result.name
+        })
+      },
+      fail: (e) => {},
+      complete: () => {}
+    });
+  },
+
+  submit: function (e) {
+
+  },
+
+  forbid: function (e) {
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/selfie/selfie.js
@@ -0,0 +1,148 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+  data: {
+    src: '', //拍照后图像路径(临时路径)
+    show: false //相机视图显示隐藏标识
+  },
+
+  // 取消/重新拍照按钮
+  cancelBtn() {
+    this.setData({ //更新数据
+      show: false
+    })
+  },
+
+  // 点击拍照按钮
+  takePhoto() {
+
+    // 创建camera上下文CameraContext对象
+    // 具体查看微信api文档
+    const ctx = wx.createCameraContext()
+
+    // 获取camera实时帧数据
+    const listener = ctx.onCameraFrame((frame) => {
+      //如果不需要则注释掉
+    })
+
+    // 实拍照片配置
+    ctx.takePhoto({
+      quality: 'high', //成像质量
+
+      success: (res) => { //成功回调
+        this.setData({
+          src: res.tempImagePath, //tempImagePath为api返回的照片路径
+          show: true
+        })
+      },
+
+      fail: (error) => { //失败回调
+        //友好提示...
+      }
+
+    })
+  },
+
+
+  // 保存图片/更改主页数据(用户最终点击确定按钮√)
+  saveImg() {
+
+    // 获取所有页面(不懂请移步下面这篇文章)
+    // https://blog.csdn.net/weixin_44198965/article/details/107821802
+    let pages = getCurrentPages()
+
+    // 当前页面-flag
+    var currentPage = '';
+
+    // 上一页-flag
+    var prevPage = '';
+
+    // 如果长度大于等于2
+    if (pages.length >= 2) { //则对上面定义的flag赋值
+
+      // 当前页
+      currentPage = pages[pages.length - 1];
+
+      // 上一页
+      prevPage = pages[pages.length - 2];
+    }
+
+    // 刷新上一页(也就是主页面)数据-包含图片路径及标识
+    if (prevPage) {
+
+      // 获取当前图片路径(用户拍下的照片)
+      var src = currentPage.data.src;
+
+      // 动态更新数据(不懂移步文章)
+      // https://blog.csdn.net/weixin_44198965/article/details/107821802 获取页面栈后 可以访问页面栈中的数据-这样达到的不同页面更新数据
+      prevPage.setData({
+        selfie: src, //照片路径
+        selfieTmp: src //照片路径
+      })
+    }
+
+    // 最后返回上一页(也就是主页)
+    wx.navigateBack({
+      delta: 1,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
